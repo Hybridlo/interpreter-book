cb6d60a24eaeb9b2eb0698115ecb043d3ddab6e0
Павел <pashanoy@gmail.com>
2024-03-07 14:13:45 +0200

    Add a basic REPL, which just prints out tokens of input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod lexer;
 mod token;
+mod repl;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello! This is the Monkey programming language!");
+    println!("Feel free to type in commands");
+    repl::start(std::io::stdin(), std::io::stdout())
 }
---
+++ src/repl/mod.rs
@@ -0,0 +1,30 @@
+use std::io::{BufRead as _, BufReader, Read, Write};
+
+use crate::{lexer::Lexer, token::Token};
+
+const PROMPT: &'static str = ">> ";
+
+pub fn start(input: impl Read, mut output: impl Write) {
+    let mut scanner = BufReader::new(input);
+    let mut line_buf = String::new();
+
+    loop {
+        write!(output, "{}", PROMPT).expect("Writing to output in REPL failed");
+        output.flush().expect("Flushing output in REPL failed");
+        let scanned = scanner.read_line(&mut line_buf).expect("Reading a line failed");
+        if scanned == 0 {
+            return
+        }
+
+        let lexer = Lexer::new(&line_buf);
+
+        for token in lexer {
+            if token != Token::Eof {
+                writeln!(output, "{:?}", token).expect("Writing to output in REPL failed");
+                output.flush().expect("Flushing output in REPL failed");
+            } else {
+                break;
+            }
+        }
+    }
+}
