d923ca04ade67a83834ae8050159a8b4b395504a
Павел <pashanoy@gmail.com>
2024-03-09 18:57:54 +0200

    Add return statements and error handling

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,5 +1,6 @@
-use crate::ast::expressions::{IfExpression, InfixOperator, PrefixExpression, PrefixOperator};
-use crate::ast::statements::Statement;
+use crate::ast::expressions::{IfExpression, InfixOperator, PrefixOperator};
+use crate::ast::statements::{BlockStatement, Statement};
+use crate::ast::Program;
 use crate::ast::{expressions::Expression, Node};
 use crate::object::{self, Object};
 
@@ -7,15 +8,39 @@ pub fn eval(node: Node) -> Option<Object> {
     match node {
         Node::Statement(stmt) => eval_statement(stmt),
         Node::Expression(expr) => eval_expression(expr),
-        Node::Program(prog) => eval_statements(prog.statements),
+        Node::Program(prog) => eval_program(prog),
     }
 }
 
-pub fn eval_statements(stmts: Vec<Statement>) -> Option<Object> {
+pub fn eval_program(prog: Program) -> Option<Object> {
     let mut res = None;
 
-    for statement in stmts {
-        res = eval_statement(statement)
+    for statement in prog.statements {
+        res = eval_statement(statement);
+
+        if let Some(Object::Return(ret)) = res {
+            return Some(*ret.0);
+        }
+        if let Some(Object::Error(err)) = res {
+            return Some(Object::Error(err));
+        }
+    }
+
+    res
+}
+
+pub fn eval_block_statement(block: BlockStatement) -> Option<Object> {
+    let mut res = None;
+
+    for statement in block.0 {
+        res = eval_statement(statement);
+
+        if let Some(Object::Return(ret)) = res {
+            return Some(Object::Return(ret));
+        }
+        if let Some(Object::Error(err)) = res {
+            return Some(Object::Error(err));
+        }
     }
 
     res
@@ -24,9 +49,17 @@ pub fn eval_statements(stmts: Vec<Statement>) -> Option<Object> {
 pub fn eval_statement(stmt: Statement) -> Option<Object> {
     match stmt {
         Statement::Let(_) => todo!(),
-        Statement::Return(_) => todo!(),
+        Statement::Return(ret) => {
+            let val = eval(ret.0.into())
+                .expect("A return statement had an invalid operand");
+            if let Object::Error(err) = val {
+                return Some(Object::Error(err))
+            }
+
+            Some(Object::Return(object::Return(Box::new(val))))
+        },
         Statement::Expression(expr) => eval_expression(expr.0),
-        Statement::Block(block) => eval_statements(block.0),
+        Statement::Block(block) => eval_block_statement(block),
     }
 }
 
@@ -37,13 +70,25 @@ pub fn eval_expression(expr: Expression) -> Option<Object> {
         Expression::Prefix(prefix_expr) => {
             let right = eval((*prefix_expr.right).into())
                 .expect("A prefix operation had an invalid operand");
+            if let Object::Error(err) = right {
+                return Some(Object::Error(err))
+            }
+
             eval_prefix_expression(prefix_expr.operator, right)?
         }
         Expression::Infix(infix_expr) => {
             let left = eval((*infix_expr.left).into())
                 .expect("An infix operation had an invalid left operand");
+            if let Object::Error(err) = left {
+                return Some(Object::Error(err))
+            }
+
             let right = eval((*infix_expr.right).into())
                 .expect("An infix operation had an invalid right operand");
+            if let Object::Error(err) = right {
+                return Some(Object::Error(err))
+            }
+
             eval_infix_expression(left, infix_expr.operator, right)?
         }
         // because we're cool like that - creating objects is cheap, unlike how they are in Go, so we don't
@@ -73,7 +118,7 @@ pub fn eval_not_operator_expression(right: Object) -> Option<Object> {
 
 pub fn eval_minus_prefix_operator_expression(right: Object) -> Option<Object> {
     let Object::Integer(object::Integer(int_val)) = right else {
-        return Some(Object::Null);
+        return Some(Object::Error(object::Error(format!("unknown operator: -{}", right.obj_type()))));
     };
 
     Some(Object::Integer(object::Integer(-int_val)))
@@ -98,7 +143,22 @@ pub fn eval_infix_expression(
         (left, InfixOperator::NotEqual, right) => {
             Some(Object::Boolean(object::Boolean(left != right)))
         }
-        _ => Some(Object::Null),
+        (left, operator, right) if left.obj_type() != right.obj_type() => {
+            Some(Object::Error(object::Error(format!(
+                "type mismatch: {} {} {}",
+                left.obj_type(),
+                operator,
+                right.obj_type()
+            ))))
+        }
+
+        // a default path
+        (left, operator, right) => Some(Object::Error(object::Error(format!(
+            "unknown operator: {} {} {}",
+            left.obj_type(),
+            operator,
+            right.obj_type()
+        )))),
     }
 }
 
@@ -121,6 +181,9 @@ pub fn eval_integer_infix_expression(
 
 pub fn eval_if_expression(if_expr: IfExpression) -> Option<Object> {
     let condition = eval((*if_expr.condition).into())?;
+    if let Object::Error(err) = condition {
+        return Some(Object::Error(err))
+    }
 
     if is_truthy(condition) {
         // annoying, but can't blanket impl From<_> for Node for inner Expression and Statement types
@@ -257,9 +320,9 @@ mod tests {
     }
 
     #[test]
-    fn test_if_else_expressions() {
+    fn test_if_else_expression() {
         let tests = [
-            ("if (true) { 10 }", Some(10_i64)),
+            ("if (true) { 10 }", Some(10)),
             ("if (false) { 10 }", None),
             ("if (1) { 10 }", Some(10)),
             ("if (1 < 2) { 10 }", Some(10)),
@@ -276,6 +339,69 @@ mod tests {
             } else {
                 assert_null_object(evaluated);
             };
+        }
+    }
+
+    #[test]
+    fn test_return_statement() {
+        let tests = [
+            ("return 10;", 10),
+            ("return 10; 9;", 10),
+            ("return 2 * 5; 9;", 10),
+            ("9; return 2 * 5; 9;", 10),
+            (
+                r#"
+                if (10 > 1) {
+                    if (10 > 1) {
+                        return 10;
+                    }
+                    
+                    return 1;
+                }
+                "#,
+                10,
+            ),
+        ];
+
+        for (input, expected_ret) in tests {
+            let evaluated = test_eval(input).unwrap();
+            assert_integer_object(evaluated, expected_ret)
+        }
+    }
+
+    #[test]
+    fn test_error_handling() {
+        let tests = [
+            ("5 + true;", "type mismatch: INTEGER + BOOLEAN"),
+            ("5 + true; 5;", "type mismatch: INTEGER + BOOLEAN"),
+            ("-true", "unknown operator: -BOOLEAN"),
+            ("true + false;", "unknown operator: BOOLEAN + BOOLEAN"),
+            ("5; true + false; 5", "unknown operator: BOOLEAN + BOOLEAN"),
+            (
+                "if (10 > 1) { true + false; }",
+                "unknown operator: BOOLEAN + BOOLEAN",
+            ),
+            (
+                r#"
+                if (10 > 1) {
+                    if (10 > 1) {
+                        return true + false;
+                    }
+                    return 1;
+                }
+                "#,
+                "unknown operator: BOOLEAN + BOOLEAN",
+            ),
+        ];
+
+        for (input, expected_err) in tests {
+            let evaluated = test_eval(input).unwrap();
+
+            let Object::Error(object::Error(err)) = evaluated else {
+                panic!("Expected `Error` object, {:?} got", evaluated);
+            };
+
+            assert_eq!(err, expected_err);
         }
     }
 }
--- src/object.rs
+++ src/object.rs
@@ -8,6 +8,22 @@ pub enum Object {
     Boolean(Boolean),
     #[display(fmt = "null")]
     Null,
+    #[display(fmt = "{_0}")]
+    Return(Return),
+    #[display(fmt = "ERROR: {_0}")]
+    Error(Error),
+}
+
+impl Object {
+    pub fn obj_type(&self) -> &'static str {
+        match self {
+            Object::Integer(_) => "INTEGER",
+            Object::Boolean(_) => "BOOLEAN",
+            Object::Null => "NULL",
+            Object::Return(_) => "RETURN_VALUE",
+            Object::Error(_) => "ERROR",
+        }
+    }
 }
 
 #[derive(Clone, Debug, Display, PartialEq, Eq)]
@@ -17,3 +33,11 @@ pub struct Integer(pub i64);
 #[derive(Clone, Debug, Display, PartialEq, Eq)]
 #[display(fmt = "{_0}")]
 pub struct Boolean(pub bool);
+
+#[derive(Clone, Debug, Display, PartialEq, Eq)]
+#[display(fmt = "{_0}")]
+pub struct Return(pub Box<Object>);
+
+#[derive(Clone, Debug, Display, PartialEq, Eq)]
+#[display(fmt = "{_0}")]
+pub struct Error(pub String);
