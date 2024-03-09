b1d25098b85084322621a23a6ca3effea41f381f
Павел <pashanoy@gmail.com>
2024-03-09 17:07:01 +0200

    Add prefix operators (`-` and `!`) to eval

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,3 +1,4 @@
+use crate::ast::expressions::{PrefixExpression, PrefixOperator};
 use crate::ast::statements::Statement;
 use crate::ast::{expressions::Expression, Node};
 use crate::object::{self, Object};
@@ -33,7 +34,10 @@ pub fn eval_expression(expr: Expression) -> Option<Object> {
     Some(match expr {
         Expression::Identifier(_) => todo!(),
         Expression::IntegerLiteral(int_literal) => Object::Integer(object::Integer(int_literal.0)),
-        Expression::Prefix(_) => todo!(),
+        Expression::Prefix(prefix_expr) => {
+            let right = eval((*prefix_expr.right).into()).expect("A prefix operation had an invalid operand");
+            eval_prefix_expression(prefix_expr.operator, right)?
+        },
         Expression::Infix(_) => todo!(),
         // because we're cool like that - creating objects is cheap, unlike how they are in Go, so we don't
         // need to optimize booleans values in singletons
@@ -43,6 +47,30 @@ pub fn eval_expression(expr: Expression) -> Option<Object> {
         Expression::Function(_) => todo!(),
         Expression::Call(_) => todo!(),
     })
+}
+
+
+pub fn eval_prefix_expression(operator: PrefixOperator, right: Object) -> Option<Object> {
+    match operator {
+        PrefixOperator::Minus => eval_minus_prefix_operator_expression(right),
+        PrefixOperator::Not => eval_not_operator_expression(right),
+    }
+}
+
+pub fn eval_not_operator_expression(right: Object) -> Option<Object> {
+    Some(match right {
+        Object::Boolean(object::Boolean(val)) => Object::Boolean(object::Boolean(!val)),
+        Object::Null => Object::Boolean(object::Boolean(true)),
+        _ => Object::Boolean(object::Boolean(false))
+    })
+}
+
+pub fn eval_minus_prefix_operator_expression(right: Object) -> Option<Object> {
+    let Object::Integer(object::Integer(int_val)) = right else {
+        return Some(Object::Null);
+    };
+
+    Some(Object::Integer(object::Integer(-int_val)))
 }
 
 #[cfg(test)]
@@ -76,7 +104,12 @@ mod tests {
 
     #[test]
     fn test_eval_integer_expression() {
-        let tests = [("5", 5), ("10", 10)];
+        let tests = [
+            ("5", 5),
+            ("10", 10),
+            ("-5", -5),
+            ("-10", -10),
+        ];
 
         for (input, expected_int) in tests {
             let evaluated = test_eval(input);
@@ -87,6 +120,23 @@ mod tests {
     #[test]
     fn test_eval_boolean_expression() {
         let tests = [("true", true), ("false", false)];
+
+        for (input, expected_bool) in tests {
+            let evaluated = test_eval(input);
+            assert_boolean_object(evaluated.unwrap(), expected_bool);
+        }
+    }
+
+    #[test]
+    fn test_bang_operator() {
+        let tests = [
+            ("!true", false),
+            ("!false", true),
+            ("!5", false),
+            ("!!true", true),
+            ("!!false", false),
+            ("!!5", true),
+        ];
 
         for (input, expected_bool) in tests {
             let evaluated = test_eval(input);
